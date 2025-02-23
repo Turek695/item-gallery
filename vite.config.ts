8dbcc3dd814729a729b3f0dbc1e804d137884043
Łukasz Turowski <Turek695@gmail.com>
2025-02-23 20:08:19 +0100

    add aliases to vite and ts configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../"),
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
